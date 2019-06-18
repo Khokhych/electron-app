b3fe441a5ad0dd9d97a2d8ea5494972cdc87247c
Bohdan <khokhych@email.ua>
2019-06-18 13:52:08 +0300

    add foto test

--- src/front.js
+++ src/front.js
@@ -1,30 +1,24 @@
-const ipcRenderer = require('electron').ipcRenderer; 
- 
+const ipcRenderer = require('electron').ipcRenderer;
+
 const formAddSubmit = document.querySelector('#form_add_submit');
 formAddSubmit.addEventListener('click', function () {
     var arg = {};
     arg.name1 = document.querySelector('input[name="name1"]').value;
     arg.name2 = document.querySelector('input[name="name2"]').value;
     arg.name3 = document.querySelector('input[name="name3"]').value;
-    ipcRenderer.send("formAddSubmit", arg); 
+    arg.foto = document.querySelector('input[name="foto"]').files[0].path;
+    ipcRenderer.send("formAddSubmit", arg);
 });
 
-window.onload = function() {
+window.onload = function () {
     ipcRenderer.send('asynchronous-message', 'ping');
-    
+
     var wrapp = document.createElement('div');
-    
+
     ipcRenderer.on('asynchronous-reply', (event, arg) => {
         wrapp.innerHTML = JSON.stringify(arg);
-        
+
     });
-    
+
     document.querySelector('body').appendChild(wrapp);
 };
-
-
-
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-const { app, BrowserWindow } = require('electron');
+const {
+  app,
+  BrowserWindow
+} = require('electron');
 const path = require('path');
 const Store = require('./store.js');
 require('./personal.js');
-const {ipcMain} = require('electron'); 
+const {
+  ipcMain
+} = require('electron');
 
 
 var Datastore = require('nedb');
-var db = new Datastore({filename : 'users'});
+var db = new Datastore({
+  filename: 'users'
+});
 db.loadDatabase();
 
-ipcMain.on("formAddSubmit",function (event, arg) {
+ipcMain.on("formAddSubmit", function (event, arg) {
   db.insert({
     name1: arg.name1,
     name2: arg.name2,
-    name3: arg.name3
+    name3: arg.name3,
+    foto: arg.foto,
   });
 });
 
@@ -22,9 +30,9 @@ var data = 0;
 ipcMain.on('asynchronous-message', (event, arg) => {
   db.find({}, function (err, docs) {
     data = docs;
-    event.sender.send('asynchronous-reply', data );
+    event.sender.send('asynchronous-reply', data);
   });
-  
+
 });
 
 
@@ -35,18 +43,33 @@ let mainWindow;
 const store = new Store({
   configName: 'user-preferences',
   defaults: {
-    windowBounds: { width: 800, height: 600 }
+    windowBounds: {
+      width: 800,
+      height: 600
+    }
   }
 });
 
 
 
-app.on('ready', function() {
-    let { width, height } = store.get('windowBounds');
-  mainWindow = new BrowserWindow({ width, height });
+app.on('ready', function () {
+  let {
+    width,
+    height
+  } = store.get('windowBounds');
+  mainWindow = new BrowserWindow({
+    width,
+    height
+  });
   mainWindow.on('resize', () => {
-    let { width, height } = mainWindow.getBounds();
-    store.set('windowBounds', { width, height });
+    let {
+      width,
+      height
+    } = mainWindow.getBounds();
+    store.set('windowBounds', {
+      width,
+      height
+    });
   });
   mainWindow.loadURL('file://' + path.join(__dirname, 'index.html'));
 
