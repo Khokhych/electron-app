3607cfdfac4bcd8c40ab1d3bb43464ff28282e76
Bohdan <khokhych@email.ua>
2019-06-28 20:29:51 +0300

    form_add_party

--- src/front.js
+++ src/front.js
@@ -6,14 +6,14 @@ myConsole.log(1);
 const formAddSubmit = document.querySelector('#form_add_submit');
 formAddSubmit.addEventListener('click', function () {
     var arg = {};
-    arg.rank = document.querySelector('select[name="rank"]').value;
-    arg.surname = document.querySelector('input[name="surname"]').value;
-    arg.name = document.querySelector('input[name="name"]').value;
-    arg.middleName = document.querySelector('input[name="middleName"]').value;
-    arg.dateOfBirth = document.querySelector('input[name="dateOfBirth"]').value;
-    arg.phoneNumbers = document.querySelector('input[name="phoneNumbers"]').value;
-    arg.milDocNum = document.querySelector('input[name="milDocNum"]').value;
-    arg.tokenNumber = document.querySelector('input[name="tokenNumber"]').value;
+    arg.rank = document.querySelector('.form_add_user select[name="rank"]').value;
+    arg.surname = document.querySelector('.form_add_user input[name="surname"]').value;
+    arg.name = document.querySelector('.form_add_user input[name="name"]').value;
+    arg.middleName = document.querySelector('.form_add_user input[name="middleName"]').value;
+    arg.dateOfBirth = document.querySelector('.form_add_user input[name="dateOfBirth"]').value;
+    arg.phoneNumbers = document.querySelector('.form_add_user input[name="phoneNumbers"]').value;
+    arg.milDocNum = document.querySelector('.form_add_user input[name="milDocNum"]').value;
+    arg.tokenNumber = document.querySelector('.form_add_user input[name="tokenNumber"]').value;
     ipcRenderer.send("formAddSubmit", arg);
 });
 
