fdf5e9fc9fc9edfc0570ea7eb5fd979ece777a6d
Bohdan Khokhych <bohdan.khokhych@gmail.com>
2019-10-17 12:33:20 +0300

    +++

--- src/front/formAddPartyPusher.js
+++ src/front/formAddPartyPusher.js
@@ -7,12 +7,27 @@
 			let values = [];
 			const form_add_party_submit = document.querySelectorAll('.form_add_party .form_add_party_input');
 			for (let i = 0; i < form_add_party_submit.length; i++) {
-				values.push({
-						name: form_add_party_submit[i].value,
-						level: form_add_party_submit[i].getAttribute('level'),
-					}
 
-				);
+				let levelValue = form_add_party_submit[i].getAttribute('level');
+				let levelArray = levelValue.split('_');
+				let levelPushArrayString = "values";
+ 
+				for (let i = 0; i < levelArray.length; i++) {
+					if (levelArray[i]) {
+						levelPushArrayString += `[${levelArray[i]}]`;
+					}
+				};
+				levelPushArrayString += ' = form_add_party_submit[i].value';
+				console.log(levelPushArrayString);
+				eval(levelPushArrayString);
+
+				// values.push({
+				// 		name: form_add_party_submit[i].value,
+				// 		level: form_add_party_submit[i].getAttribute('level'),
+				// 	}
+
+				// );
+
 			}
 			arg.values = values;
 			ipcRenderer.send("form_add_party_submit", arg);
